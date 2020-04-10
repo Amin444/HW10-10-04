a45c462cf6bf85e6aaef2d55c8cac377bc2d5d33
unknown <aminmirzoev123@gmail.com>
2020-04-11 00:06:36 +0500

    доделал вторую задачу и еще раз изменил 1 задачу )

--- Program.cs
+++ Program.cs
@@ -32,8 +32,32 @@ namespace HW10_10_04
                 d.SaveDocument();
                 d.EditDocument();
             }
+            
+             System.Console.WriteLine("Если вы нажмете на '1' то вы зайдете на параметры Player\n Если же хотите увидеть RecordPlayer нажмите '3' ");
+                var s=int.Parse(Console.ReadLine());
+                if(s==1)
+                {
+                  Player P=new Player();
+                    P.Pause();
+                    P.Play();
+                    P.stop();
+                   
+                }
+               else if(s==3)
+                {
+                    RecordPlay R =new RecordPlay();
+                     R.Pause();
+                     R.Record();
+                     R.Stop();
+                   
+
+                }
+                else if(s!=1|| s!=3)
+                 {
+                     System.Console.WriteLine("Error! Вы не ввели ни одну из перечисленых команд \n пожалуста введите ''1 или '3'");
+                 }
             Console.ReadKey();
-            }   
+              
 
         }
     }
@@ -76,18 +100,9 @@ namespace HW10_10_04
 
     interface IPalayer
     {
-        public void Play()
-        {
-
-        }
-        public void Pause()
-        {
-
-        }
-        public void stop()
-        {
-
-        }
+         void Play();
+         void Pause();
+         void stop();
     }
     interface IRecodable
     {
@@ -95,20 +110,51 @@ namespace HW10_10_04
            void Pause();
             void Stop();
     }
-     
-        
-
-        
-     
-
-
-        class Player:IPalayer
+    class Player : IPalayer
+    {
+        public void Pause()
         {
-
+            System.Console.WriteLine("остановить");
         }
 
-         class Player1:IRecodable  
-         {
+        public void Play()
+        {
+            System.Console.WriteLine("играть");
+        }
 
-         }
+        public void stop()
+        {
+            System.Console.WriteLine("прекратить какое то");
+        }
+    }
+    class RecordPlay : IRecodable
+    {
+       public void Pause()
+    {
+        Console.WriteLine("I've paused music");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("I've stopped music");
+    }
+    public void Record()
+    {
+        Console.WriteLine("I'm recording");
+    }
+    
+
+  }
 }
+
+}
+
+
+
+
+
+
+
+
+
+
